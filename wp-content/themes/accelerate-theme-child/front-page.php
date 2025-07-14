7e852134dee703f744fea35d702c0acf79466772
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2025-07-14 22:14:12 +0200

    Added blog post to the main page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -18,7 +18,8 @@
 
 get_header(); ?>
 	<div id="primary" class="home-page hero-content">
-		<div class="main-content" role="main">
+
+	<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
@@ -26,4 +27,21 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+
+	<!-- RECENT BLOG POST -->
+		<section class="recent-posts">
+            <div class="site-content">
+  <div class="blog-post">
+   <h4>From the Blog</h4>
+    <?php query_posts('posts_per_page=1'); ?>
+     <?php while ( have_posts() ) : the_post(); ?>
+      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+       <?php the_excerpt(); ?> 
+     <?php endwhile; ?> 
+    <?php wp_reset_query(); ?>
+   </div>
+ </div>
+</section>
+		
+	
 <?php get_footer(); ?>
